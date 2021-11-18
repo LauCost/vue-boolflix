ee6d28e28c1a6cdc52a4d5e8b98dd09f8b0bb018
Laurentiu <lcostin40@gmail.com>
2021-11-18 12:29:40 +0100

    Aggiunto codice per filtrare i film (SEARCH NON FUNZIONANTE)

---
+++ src/components/Content Main/Film.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="cont">
+    <h2>
+      {{ Titolo }}
+    </h2>
+    <h3>
+      {{ NomeOriginale }}
+    </h3>
+    <p>
+      {{ LinguaOriginale }}
+    </p>
+    <p>
+      {{ Voto }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    Titolo: String,
+    NomeOriginale: String,
+    LinguaOriginale: String,
+    Voto: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.cont {
+  width: 50%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 20px auto;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    <ul>
-      <li></li>
-    </ul>
-  </div>
+  <main>
+    <Movie
+      v-for="film in movies"
+      :key="film.id"
+      :Titolo="film.title"
+      :NomeOriginale="film.original_title"
+      :LinguaOriginale="film.original_language"
+      :Voto="film.vote_average"
+    />
+  </main>
 </template>
 
 <script>
 import axios from "axios";
+import Movie from "./Content Main/Film.vue";
 export default {
   data() {
     return {
@@ -15,13 +21,17 @@ export default {
     };
   },
 
+  components: {
+    Movie,
+  },
+
   mounted() {
     axios
       .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=58ebf4fc2efccc2902a38beabf175722&language=en-US&query=&page=1&include_adult=false"
+        "https://api.themoviedb.org/3/search/movie?api_key=58ebf4fc2efccc2902a38beabf175722&language=en-US&query=h&page=1&include_adult=false"
       )
       .then((r) => {
-        this.movies = r.data;
+        this.movies = r.data.results;
       });
   },
 };
