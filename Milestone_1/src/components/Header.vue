b89dea3f37dc89268d034640c9252e4d5ccb803a
Laurentiu <lcostin40@gmail.com>
2021-11-18 11:43:18 +0100

    Aggiunto search bar

--- Milestone_1/src/components/Header.vue
+++ Milestone_1/src/components/Header.vue
@@ -1,8 +1,36 @@
 <template>
-  <Header> </Header>
+  <Header>
+    <div class="search">
+      <input type="text" placeholder="Search.." />
+      <button>Search</button>
+    </div>
+  </Header>
 </template>
 
 
 
 
+<style lang="scss">
+header {
+  background-color: #2e3a46;
+}
 
+.search {
+  display: flex;
+  justify-content: center;
+  input {
+    margin: 30px 0;
+    width: 50%;
+    padding: 20px 0;
+    font-size: 25px;
+  }
+
+  button {
+    margin: 30px 10px;
+    padding: 30px;
+    border: none;
+    background-color: #fff;
+    font-size: 25px;
+  }
+}
+</style>
