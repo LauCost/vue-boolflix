4494de7508c8c16ed7f4c3196c7cce7007b1eef8
Laurentiu <lcostin40@gmail.com>
2021-11-18 12:13:12 +0100

    Aggiunto API

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      movies: [],
+    };
+  },
+
+  mounted() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/search/movie?api_key=58ebf4fc2efccc2902a38beabf175722&language=en-US&query=&page=1&include_adult=false"
+      )
+      .then((r) => {
+        this.movies = r.data;
+      });
+  },
+};
+</script>
