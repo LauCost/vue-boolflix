5bd8d0acfc75840cc3b1d0a292e17abcd3794e72
Laurentiu <lcostin40@gmail.com>
2021-11-20 15:45:35 +0100

    Aggiunto stelline al voto. Terminato Minestone 3

--- src/components/Content Main/Film.vue
+++ src/components/Content Main/Film.vue
@@ -8,10 +8,18 @@
       {{ NomeOriginale }}
     </h3>
     <img :src="image" alt="" />
-    <p v-if="LinguaOriginale == 'en'"><flag iso="gb" /> | {{ Voto }}</p>
-    <p v-else-if="LinguaOriginale == 'it'"><flag iso="it" /> | {{ Voto }}</p>
-    <p v-else-if="LinguaOriginale == 'cn'"><flag iso="cn" /> | {{ Voto }}</p>
-    <p v-else>{{ LinguaOriginale }} | {{ Voto }}</p>
+    <p v-if="LinguaOriginale == 'en'"><flag iso="gb" /></p>
+    <p v-else-if="LinguaOriginale == 'it'"><flag iso="it" /></p>
+    <p v-else-if="LinguaOriginale == 'cn'"><flag iso="cn" /></p>
+    <p v-else>{{ LinguaOriginale }}</p>
+    <p>
+      {{ Voto }}
+    </p>
+    <p>
+      <span v-for="(star, i) in getRightVote(Voto)" :key="i">
+        <i :class="star"></i>
+      </span>
+    </p>
   </div>
 </template>
 
@@ -23,6 +31,24 @@ export default {
     LinguaOriginale: String,
     Voto: Number,
     image: String,
+  },
+
+  methods: {
+    getRightVote(number) {
+      let int = Math.ceil(number / 2);
+      const starFull = "fas fa-star";
+      const starEmpty = "far fa-star";
+      const listStar = [];
+      while (listStar.length < 5) {
+        if (int > 0) {
+          listStar.push(starFull);
+          int--;
+        } else {
+          listStar.push(starEmpty);
+        }
+      }
+      return listStar;
+    },
   },
 };
 </script>
